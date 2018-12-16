477ad723c281cdc700e4f8ddd954a1bc7b1a785c
Omer Katzir <omerkatzir@gmail.com>
2018-12-16 19:15:08 +0200

    Update sketchForWF.js

--- threejs/JS/sketchForWF.js
+++ threejs/JS/sketchForWF.js
@@ -166,7 +166,7 @@ function instantiateGif() {
   } else {
     min = -2;
     max = 2;
-    pos = new THREE.Vector3(getRandomIntArbitrary(0, 1), getRandomIntArbitrary(min - 1, max + 1), 0);
+    pos = new THREE.Vector3(getRandomIntArbitrary(0, 1), getRandomIntArbitrary(0, max + 2), 0);
   }
 
   //handels gifs created in same X or Y
